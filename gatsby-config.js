b297942c4e57dd028d8f2483844d1be8c7744361
LaurenMF <lfasig@iu.edu>
2021-09-22 16:48:41 -0400

    'update'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~lfasig/todo-list",
     title: "EllF Todo List",
   },
   plugins: [],
+  pathPrefix: '/~lfasig/todo-list'
 };
